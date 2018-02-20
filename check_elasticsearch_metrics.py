94689360a12cf960d4f2f134e9bbcdf2498d6634
Ilya Shubkin <ishubkin@mustapp.me>
2018-02-20 13:18:53 +0300

    add check_elasticsearch_metrics plugin

---
+++ check_elasticsearch_metrics.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+
+import sys
+import argparse
+import logging
+from datetime import datetime, timedelta
+
+from elasticsearch import Elasticsearch, exceptions
+from elasticsearch_dsl import Search, A
+
+from enum import Enum
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+version = "0.1"
+
+
+class NagiosReturnCodes(Enum):
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    UNKNOWN = 3
+
+
+def parse_args(argv):
+    arg_parser = argparse.ArgumentParser(description="Obtains metrics from elasticsearch to power Icinga alerts",
+                                         formatter_class=argparse.RawDescriptionHelpFormatter,
+                                         epilog="Error codes:\n"
+                                                "\t0: Everything OK, check passed\n"
+                                                "\t1: Warning threshold breached\n"
+                                                "\t2: Critical threshold breached\n"
+                                                "\t3: Unknown, encountered an error querying elasticsearch\n")
+
+    # required options
+    arg_parser.add_argument("-c", "--critical", action="store", type=float, required=True, help="critical threshold")
+    arg_parser.add_argument("--host", action="store", default="localhost", required=True, help="elasticsearch host (default: localhost)")
+    arg_parser.add_argument("-i", "--indices_count", action="store", type=int, default=2,
+                            help="the number of indices to go back through (default: 2)")
+    arg_parser.add_argument("--index_prefix", action="store", default="logstash", help="index prefix (default: logstash")
+    arg_parser.add_argument("-n", "--index_pattern", action="store", default="{prefix}-{yyyy}.{mm}.{dd}",
+                            help="the pattern expects months and years and can take a prefix and days, e.g: metrics-{yyyy}.{mm}")
+    arg_parser.add_argument("-s", "--seconds", action="store", type=int, required=True, help="number of seconds from now to check")
+    arg_parser.add_argument("-q", "--query", action="store", required=True, help="the query to run in elasticsearch")
+    arg_parser.add_argument("-w", "--warning", action="store", type=float, required=True, help="warning threshold")
+
+    # optional options
+    arg_parser.add_argument("--aggregation_name", action="store", help="aggregation name")
+    arg_parser.add_argument("--aggregation_type", action="store", choices=("significant_terms",), help="aggregation type")
+    arg_parser.add_argument("--aggregation_field", action="store", help="the name of the field to aggregate")
+    arg_parser.add_argument("--aggregation_result_bucket_key", action="append", help="specify aggregation bucket keys (repeatable argument)")
+    arg_parser.add_argument("--aggregation_result_type", action="store", choices=("count", "percentage"), default="count",
+                            help="aggregation result type (default: count)")
+    arg_parser.add_argument("-d", "--include_day", action="store_true", help="include the day in elasticsearch index")
+    arg_parser.add_argument("-p", "--port", action="store", type=int, default=9200, help="elasticsearch port (default: 9200)")
+    arg_parser.add_argument("-r", "--reverse", action="store_true", help="reverse threshold (so amounts below threshold values will alert)")
+    arg_parser.add_argument("--debug", action="store_true", default=False, help="print debug messages")
+    arg_parser.add_argument("--version", action="version", version='%(prog)s {version}'.format(version=version))
+
+    return arg_parser.parse_args(argv)
+
+
+def build_indices(indices_count=2, index_pattern="{prefix}-{yyyy}.{mm}.{dd}", index_prefix="logstash"):
+    indices = []
+    today = datetime.now().date()
+
+    for i in range(indices_count):
+        t = today - timedelta(days=i)
+        indices.append(index_pattern.format(prefix=index_prefix,
+                                            yyyy=t.year,
+                                            mm="{:02d}".format(t.month),
+                                            dd="{:02d}".format(t.day)))
+
+    return ",".join(indices)
+
+
+def calc_percent(part, whole):
+    if whole <= int(0):
+        return 0
+
+    return round(part * 100 / whole, 2)
+
+
+def get_alert_status(args, value):
+    if args.reverse:
+        if value <= args.critical:
+            return NagiosReturnCodes.CRITICAL.value
+        if value <= args.warning:
+            return NagiosReturnCodes.WARNING.value
+    else:
+        if value >= args.critical:
+            return NagiosReturnCodes.CRITICAL.value
+        if value >= args.warning:
+            return NagiosReturnCodes.WARNING.value
+
+    return NagiosReturnCodes.OK.value
+
+
+def need_aggregate(args):
+    return args.aggregation_name and args.aggregation_type and args.aggregation_field
+
+
+def execute_elastic_query(args):
+    logger.debug(args)
+
+    query = args.query
+    host = args.host
+    port = args.port
+    from_time = "now-{seconds}s".format(seconds=args.seconds)
+    aggregate = need_aggregate(args)
+
+    index = build_indices(indices_count=args.indices_count,
+                          index_pattern=args.index_pattern,
+                          index_prefix=args.index_prefix)
+
+    client = Elasticsearch(hosts=["{}:{}".format(host, port)])
+
+    s = Search(using=client, index=index) \
+        .query("query_string", query=query, analyze_wildcard=True) \
+        .query("range", **{"@timestamp": {"gte": "{}".format(from_time)}})
+
+    if aggregate:
+        s.aggs.bucket(args.aggregation_name, A(args.aggregation_type, field=args.aggregation_field))
+
+    return s.execute()
+
+
+def handle_elastic_response(args, response):
+    result = 0
+
+    if need_aggregate(args):
+        res_aggregation = {}
+
+        for bucket in response.aggregations[args.aggregation_name].buckets:
+            res_aggregation.update({bucket.key:
+                                        {"count": bucket.doc_count,
+                                         "percentage": calc_percent(bucket.doc_count,
+                                                                    response.aggregations[args.aggregation_name].doc_count)}
+                                    })
+        logger.debug(res_aggregation)
+
+        # does not metter result_type is percentage or count
+        for field in args.aggregation_result_bucket_key:
+            result += res_aggregation.get(field).get(args.aggregation_result_type) if res_aggregation.get(field) else 0
+    else:
+        result = response.hits.total
+
+    return result
+
+
+def main(argv):
+    args = parse_args(argv)
+
+    if args.debug:
+        logging.getLogger().setLevel(level=logging.DEBUG)
+
+    result = 0
+    try:
+        logger.debug("args: {}".format(args))
+
+        response = execute_elastic_query(args)
+        result = handle_elastic_response(args, response)
+    except exceptions.ElasticsearchException as e:
+        logger.error("Got elasticsearch exception: {}".format(e))
+        exit(NagiosReturnCodes.UNKNOWN.value)
+
+    logger.debug("result: {}".format(result))
+
+    alert_status = get_alert_status(args, result)
+    logger.info("Exited with: {alert_status}, Current Value: {value}, Critical: {critical}, Warning: {warning}"
+                .format(alert_status=alert_status, value=result, critical=args.critical, warning=args.warning))
+    exit(alert_status)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tests/test_check_elasticsearch_metrics.py
@@ -0,0 +1,307 @@
+import pytest
+import sure
+
+import argparse
+import datetime
+
+import check_elasticsearch_metrics
+
+
+class TestBuildIndices:
+    class StubDatetime:
+        @staticmethod
+        def now():
+            return datetime.datetime(2018, 1, 15)
+
+    @pytest.fixture(scope="class")
+    def mock_datetime(self):
+        check_elasticsearch_metrics.datetime = self.StubDatetime
+        yield 1
+        check_elasticsearch_metrics.datetime = datetime.datetime
+
+    def test_base_case_positive(self, mock_datetime):
+        index = check_elasticsearch_metrics.build_indices(indices_count=2, index_pattern="{prefix}-{yyyy}.{mm}.{dd}", index_prefix="logstash")
+
+        index.should.be.equal("logstash-2018.01.15,logstash-2018.01.14")
+
+    def test_defaults(self, mock_datetime):
+        index = check_elasticsearch_metrics.build_indices()
+
+        index.should.be.equal("logstash-2018.01.15,logstash-2018.01.14")
+
+    def test_indices_count_eq_1(self):
+        index = check_elasticsearch_metrics.build_indices(indices_count=1)
+        index.should.be.equal("logstash-2018.01.15")
+
+    def test_indices_count_gt_1(self):
+        index = check_elasticsearch_metrics.build_indices(indices_count=2)
+        index.should.be.equal("logstash-2018.01.15,logstash-2018.01.14")
+
+        index = check_elasticsearch_metrics.build_indices(indices_count=3)
+        index.should.be.equal("logstash-2018.01.15,logstash-2018.01.14,logstash-2018.01.13")
+
+    def test_index_pattern_constant(self):
+        index = check_elasticsearch_metrics.build_indices(indices_count=1, index_pattern="const", index_prefix="xxx")
+        index.should.be.equal("const")
+
+    def test_index_pattern_use_prefix(self):
+        index = check_elasticsearch_metrics.build_indices(indices_count=1, index_pattern="pattern-{prefix}", index_prefix="xxx")
+        index.should.be.equal("pattern-xxx")
+
+
+# class TestParseArgs:
+#     def test(self):
+#         assert 0
+
+
+class Stub(dict):
+    MARKER = object()
+
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])
+        else:
+            raise TypeError('expected dict')
+
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, Stub):
+            value = Stub(value)
+        super(Stub, self).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        found = self.get(key, Stub.MARKER)
+        if found is Stub.MARKER:
+            found = Stub()
+            super(Stub, self).__setitem__(key, found)
+        return found
+
+    __setattr__, __getattr__ = __setitem__, __getitem__
+
+
+class TestHandleElasticResponse:
+
+    def test_no_aggregation(self):
+        response = Stub({"hits": {"total": 500}})
+        # response.hits.total = 500
+
+        args = argparse.Namespace(aggregation_name=None, aggregation_type=None, aggregation_field=None)
+
+        result = check_elasticsearch_metrics.handle_elastic_response(args, response)
+        result.should.be.equal(500)
+
+    @pytest.fixture(scope="class",
+                    params=[
+                        {
+                            "args": {
+                                "aggregation_result_bucket_key": ["WARN"],
+                                "aggregation_result_type": "count"
+                            },
+                            "expected_result": 260
+                        },
+                        {
+                            "args": {
+                                "aggregation_result_bucket_key": ["WARN", "ERROR"],
+                                "aggregation_result_type": "count"
+                            },
+                            "expected_result": 1320
+                        },
+                        {
+                            "args": {
+                                "aggregation_result_bucket_key": ["WARN"],
+                                "aggregation_result_type": "percentage"
+                            },
+                            "expected_result": 12.0
+                        },
+                        {
+                            "args": {
+                                "aggregation_result_bucket_key": ["WARN", "ERROR"],
+                                "aggregation_result_type": "percentage"
+                            },
+                            "expected_result": 60.94
+                        }
+                    ],
+                    ids=["single result-bucket-key, count result-type",
+                         "multi result-bucket-key, count result-type",
+                         "single result-bucket-key, percentage result-type",
+                         "multi result-bucket-key, percentage result-type", ])
+    def aggregation_fixture(self, request):
+        args = argparse.Namespace(aggregation_name="elastic-plugin-tests",
+                                  aggregation_type="significant_terms",
+                                  aggregation_field="level.raw",
+                                  **request.param['args'])
+        request.param["args"] = args
+
+        response = Stub(
+            {
+                "aggregations": {
+                    args.aggregation_name: {
+                        "doc_count": 2166,
+                        "bg_count": 2950965,
+                        "buckets": [
+                            Stub({
+                                "key": "WARN",
+                                "doc_count": 260,
+                                "score": 10.642715055124022,
+                                "bg_count": 14992
+                            })
+                            ,
+                            Stub({
+                                "key": "ERROR",
+                                "doc_count": 1060,
+                                "score": 10.642715055124022,
+                                "bg_count": 14992
+                            })
+                            ,
+                            Stub({
+                                "key": "INFO",
+                                "doc_count": 846,
+                                "score": 3.289777164939534,
+                                "bg_count": 425830
+                            })
+                        ]
+                    }
+                }
+            })
+
+        return response, request.param
+
+    def test_has_aggregation_by_count(self, aggregation_fixture):
+        response, req_params = aggregation_fixture
+        args = req_params["args"]
+        expected_result = req_params["expected_result"]
+
+        result = check_elasticsearch_metrics.handle_elastic_response(args, response)
+        result.should.be.equal(expected_result)
+
+
+class TestGetAlertStatus:
+
+    def test_value_lt_warning(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=0)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.OK.value)
+
+    def test_value_equal_warning(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=2)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+    def test_value_gt_warning_and_lt_critical(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=3)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+    def test_value_eq_critical(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=15)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.CRITICAL.value)
+
+    def test_value_gt_critical(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=16)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.CRITICAL.value)
+
+    def test_value_gt_warning_reverse_true(self):
+        args = argparse.Namespace(critical=2,
+                                  warning=15,
+                                  reverse=True
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=16)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.OK.value)
+
+    def test_value_eq_warning_reverse_true(self):
+        args = argparse.Namespace(critical=2,
+                                  warning=15,
+                                  reverse=True
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=15)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+    def test_value_lt_warning_and_gt_critical_reverse_true(self):
+        args = argparse.Namespace(critical=2,
+                                  warning=15,
+                                  reverse=True
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=10)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+    def test_value_eq_critical_reverse_true(self):
+        args = argparse.Namespace(critical=2,
+                                  warning=15,
+                                  reverse=True
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=2)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.CRITICAL.value)
+
+    def test_value_lt_critical_reverse_true(self):
+        args = argparse.Namespace(critical=2,
+                                  warning=15,
+                                  reverse=True
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=1)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.CRITICAL.value)
+
+    def test_decimal_values(self):
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=1.9)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.OK.value)
+
+        args = argparse.Namespace(critical=15,
+                                  warning=2.0,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=2)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=2.0)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.WARNING.value)
+
+        args = argparse.Namespace(critical=15,
+                                  warning=2,
+                                  reverse=False
+                                  )
+
+        alert_status = check_elasticsearch_metrics.get_alert_status(args, value=15.1)
+        alert_status.should.be.equal(check_elasticsearch_metrics.NagiosReturnCodes.CRITICAL.value)
+
+# class TestCheckExitCode:
+#     def test(self):
+#         assert 0
